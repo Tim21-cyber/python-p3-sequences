269653ad811192f2d45ba136ce593a47e9b99ef9
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-08-06 15:51:03 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+     print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, length):
+            next_value = fib_sequence[i - 1] + fib_sequence[i - 2]
+            fib_sequence.append(next_value)
+        print(fib_sequence)
